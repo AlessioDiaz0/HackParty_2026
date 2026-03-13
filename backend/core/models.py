095535c55c068673434479b354f03e0670f5970c
Matteo Geusa <118559552+MatteoGeusa@users.noreply.github.com>
2026-03-13 21:56:26 +0100

    adding fix

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Ticket(models.Model):
+    objects = models.Manager()
     text = models.TextField()
     category = models.CharField(max_length=50)
     confidence = models.CharField(max_length=20)
@@ -8,4 +9,4 @@ class Ticket(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.category} - {self.text[:30]}"
+        return f"{self.category} - {str(self.text)[:30]}"
